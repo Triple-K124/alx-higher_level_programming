5ee99624a076fe20edd30289683c0afb0566381e
Kalu <kalukalu526@gmail.com>
2023-03-10 16:09:30 +0000

    TAsk

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import sys
+
+args = sys.argv[1:]
+num_args = len(args)
+
+if num_args == 0:
+    print("Number of argument(s): 0.")
+    print(".")
+else:
+    print("Number of argument(s): {}.".format(num_args))
+    print("Arguments:")
+    for i, arg in enumerate(args):
+        print("{}: {}".format(i+1, arg))
