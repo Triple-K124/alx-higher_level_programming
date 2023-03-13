96c1b7816bc1f4909c6fa2ee8e89221d4e168fd9
Kalu <kalukalu526@gmail.com>
2023-03-13 14:49:44 +0000

    Task

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == " ":
-        return
+        sentence[0] = None
     length = len(sentence)
     first = sentence[0]
     answer = (length, first)
