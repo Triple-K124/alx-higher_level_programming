92f39ddd2cc8cb98e66a3c54b3b7152a44d5cf0c
Kalu <kalukalu526@gmail.com>
2024-01-05 15:38:33 +0000

    Task

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 from add_0 import add
 
-def my_calculation():
+if __name__ == "__main__":
 	a = 1
 	b = 2
-
 	result = add(a, b)
 	print(f"{a} + {b} = {result}")
-
-if __name__ == "__main__":
-	my_calculation()
