ad85572346c7a392d4a4958f5033cdbeea7b33b2
Kalu <kalukalu526@gmail.com>
2024-01-08 17:21:39 +0000

    Task

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,15 +2,15 @@
 import sys
 
 if __name__ != "__main__":
-	exit()
+    exit()
 
 argument_count = len(sys.argv) - 1
 
 i = 0
 result = 0
 for argument in sys.argv:
-	if i != 0:
-		result += int(argument)
-	else:
-		i += 1
+    if i != 0:
+        result += int(argument)
+    else:
+        i += 1
 print("{:d}".format(result))
