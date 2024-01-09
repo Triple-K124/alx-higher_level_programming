3928f1d00351f592b1be2a35541f7fdb00d91878
Kalu <kalukalu526@gmail.com>
2024-01-09 17:25:23 +0000

    Task

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    """Prints each integer in a list on a seperate line without importing modules or casting an integer"""
     for num in my_list:
         print("{:d}".format(num))
